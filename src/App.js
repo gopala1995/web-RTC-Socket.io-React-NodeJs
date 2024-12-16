0cc3b809d865a881c7669e526e87d9615c71ce5e
Gopala <ggopala1904@gmail.com>
2024-12-16 12:27:52 +0530

    first commut

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Welcome to Web RTC</header>
+    </div>
+  );
+}
+
+export default App;
